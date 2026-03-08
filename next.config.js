76f5faad4c7d879cbb77c48435f68f88fb992987
mahad002 <mahad112002@gmail.com>
2026-03-08 17:44:31 +0500

    ..

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
       { protocol: 'https', hostname: 'brandlogos.net', pathname: '/**' },
       { protocol: 'https', hostname: 'inspirovix.s3.us-east-2.amazonaws.com', pathname: '/**' },
       { protocol: 'https', hostname: 'cdn-icons-png.flaticon.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'emoti-verse.vercel.app', pathname: '/**' },
+      { protocol: 'https', hostname: 'www.gwm-pakistan.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'guruisonline.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'execchauffeurgroup.co.uk', pathname: '/**' },
+      { protocol: 'https', hostname: 'www.housingdisrepaircompensations.co.uk', pathname: '/**' },
     ],
   },
 };
